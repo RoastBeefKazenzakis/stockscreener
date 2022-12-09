bb7ee5902ce8acc475b8ea35d72f6c2ae56c46bb
Roast Beef Kazenzakis <horatiohawk@yahoo.com>
2022-12-08 21:50:53 -0500

    iterating through all tickers, discovering unreliability in API data.

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ import pandas
 
 # list all symbols
 tickers = finpie.nasdaq_tickers()
-print(tickers)
+#print(tickers)
+# now that we have all symbols, what will we do with them?
+# have to decide on initial criteria to screen on.
+def screenStock(ticker, criterion):
+    fd = finpie.Fundamentals(ticker)
+    print('Looking for ' + criterion + ' in ' + ticker + '...')
+    screenedValue = str(fd.profile())
+    print("Found " + criterion + " of " + screenedValue)
+
+#stock = input("Enter a stock symbol for more information: ")
+
+for stock in tickers['Symbol']:
+    screenStock(stock, "profile")
+    #print(stock)
 
 #screener.printStockFinpie()
